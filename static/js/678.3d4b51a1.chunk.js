a792af84cd7c9bc72a3072a0fbec8ec159c170df
CarlosAmul <74468601+CarlosAmul@users.noreply.github.com>
2022-03-30 06:57:53 +0000

    Deploying to gh-pages from @ CarlosAmul/pagetest@6df1ce037ba707c50e0d189af8f1b9e5d04c0ba6 🚀

---
+++ static/js/678.3d4b51a1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_minimal_minimal_kit_react=self.webpackChunk_minimal_minimal_kit_react||[]).push([[678],{9678:function(i,a,e){e.r(a);var m=e(2015);a.default=m.g}}]);
+//# sourceMappingURL=678.3d4b51a1.chunk.js.map
